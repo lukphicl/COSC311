0abc311067abdb8408515f9562a9a8a2b1280bdc
lukphicl <lukphicl@umich.edu>
2018-09-16 20:40:03 -0400

    Update LinkedList.java
    Fixed deleteMore function, cleaned up formatting, made neat

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,8 @@
-// Luke Clover
-// HW 0911
-// https://github.com/lukphicl/COSC311/blob/master/LinkedList.java
+/* Luke Clover
+   HW 0911
+   https://github.com/lukphicl/COSC311/blob/master/LinkedList.java
+   Below, the code I cannibalized for my insert function
+   https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/ */
 
 package linkedlist;
 
@@ -12,27 +14,32 @@ public class LinkedList {
     {
         double num;
         node next;
-        node (double p) {num = p; next = null;}
+        public node()
+        {
+            num = 0;
+            next = null;
+        }
+        public node(double p) 
+        {
+            num = p; 
+            next = null;
+        }
     }
     
     public void insert (double x)
     {
-        node newNode = new node(x);
         if (head == null) // Creates head node if list is empty
         {
             head = new node(x);
             return;
-        }
-        
-        // Unsorted insert, new node will always be added to end
-        newNode.next = null; 
+        } 
         
         // Traverses to end of list, adds new node
         node lastNode = head;
         while (lastNode.next != null)
             lastNode = lastNode.next;
         
-        lastNode.next = newNode;
+        lastNode.next = new node(x);
         return;
     }   
     
@@ -50,44 +57,42 @@ public class LinkedList {
     public double findAvg()
     {
         int i = 0;
-        node p = head;
         double total = 0, mean = 0;
         
-        while (p != null)
-        {   
-            total = total + p.num;
+        for (node p = head; p != null; p = p.next)
+        {
+            total += p.num;
             ++i;
-            p = p.next;
         }
         mean = total/i;
 
         return mean;    
     }
     
-    public void deleteLess(double avg)
+    public void deleteMore(double avg)
     {
         node temp = head;
-        
-        if (head == null) // If list is empty
+        node prev = null;
+    
+        if(head == null) // If list is empty 
         {
-            System.out.println("Empty List!");
+            System.out.println("Empty list!");
             return;
         }
-        
-        if (head != null && head.num <= avg) // If head node is to be removed
-            head = head.next;
 
-        // Parses through list, removes every occurrence
-        while (temp != null && temp.next != null)
+        if (head != null && head.num > avg) // If head node needs to be deleted
         {
-            if (temp.next.num <= avg)
-            {
-                System.out.println("Removing " + temp.next.num);
-                temp.next = temp.next.next;
-            } else
-            {
-                temp = temp.next;
-            }
+            head = head.next;
+            temp = head;
+        }
+
+        while(temp !=null) // Every remaining occurrence to be removed
+        {
+            if(temp.num > avg)
+                prev.next = temp.next; 
+            else 
+                prev = temp;
+            temp = temp.next;
         }
         
     }
@@ -100,13 +105,12 @@ public class LinkedList {
         for (int i = 0; i < arr.length; ++i) // Creates list
             myList.insert(arr[i]);
         
-        System.out.print("Initial list: ");
+        System.out.print("Your initial list: ");
         myList.printList();
         System.out.println("Your average is: " + myList.findAvg());
-        myList.deleteLess(myList.findAvg());
+        myList.deleteMore(myList.findAvg());
         System.out.print("Your final list: ");
         myList.printList();
-
     }
     
 }
